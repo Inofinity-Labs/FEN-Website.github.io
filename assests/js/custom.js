4babddafa81d823c7fcc45bec39777cac00a6c4f
Pubudu Wijekoon <37551112+pubudu-wijekoon@users.noreply.github.com>
2021-04-21 16:37:50 +0530

    navbar and solution

--- assests/js/custom.js
+++ assests/js/custom.js
@@ -1,4 +1,46 @@
-//loader
+//************************Get Current Page Details for Navbar
+$(function () {
+  setNavigation();
+});
+function setNavigation() {
+  var path = window.location.pathname;
+  path = path.slice(1);
+  //path = path.replace(/\/$/, '');
+  //path = decodeURIComponent(path);
+  //alert(path);
+  $(".navbar a").each(function () {
+      var href = $(this).attr('href');
+      //alert(href);
+      if (path.substring(0, href.length) === href) {
+          $(this).addClass('nav-active');
+          $(this).addClass('sub-active');
+          
+      }
+      //For Solution
+      if (path == 'solar-projects.html' || path == 'solar-roof.html' || path == 'concept-project.html'  || path == 'permit-approval.html' || path == 'consulting-design.html' || path =='operation-maintainance.html'){
+        $('#solution-dropdown').show();  
+        $('#solution').addClass('nav-active');
+        
+      }
+      //For Product and Partners
+      if (path == 'solar-module.html' || path == 'inverters.html' || path == 'mounting-system.html'  || path == 'rest-component.html'){
+        $('#project-dropdown').show();  
+        $('#product').addClass('nav-active');
+      }
+      //For Projects
+      if (path == 'projects.html' || path == 'project-rooftop.html'|| path == 'project-roofrental.html'|| path == 'project-ground-m.html'|| path == 'project-design.html'){
+        $('#projects-dropdown').show();
+        $('#project').addClass('nav-active');
+      }
+      //For About
+      if (path == 'about.html'){
+        $('#project-dropdown').show();  
+      }
+      
+  });
+}
+
+//************************loader
 let spinnerWrapper = document.querySelector('.spinner-wrapper');
     
         window.addEventListener('load', function () {
@@ -24,12 +66,12 @@ $(window).scroll(function() {
 //scrolling function
   $(window).scroll(function() {
     if ($(document).scrollTop() > 100) {
-      $('.navbar-d').addClass('color-change');
-      $('.nav-content-center').addClass('color-change');
+      $('#navbar-d').addClass('color-change');
+      //$('.nav-content-center').addClass('color-change');
       $('.nav-small').css({'display':'block','z-index': '2','left': '20px'})
     } else {
-      $('.navbar-d').removeClass('color-change');
-      $('.nav-content-center').removeClass('color-change');
+      $('#navbar-d').removeClass('color-change');
+      //$('.nav-content-center').removeClass('color-change');
       $('.nav-small').css({'display':'none'})
     }
   });
@@ -45,19 +87,20 @@ function clickfunction() {
     var checkBox = document.getElementById("menu-check");
     if (checkBox.click){
         console.log('checked');
-        $('.navbar-d').removeClass('color-change');
-        $('.nav-content-center').removeClass('color-change');
+        $('#navbar-d').removeClass('color-change');
+        //$('.nav-content-center').removeClass('color-change');
         $('.nav-small').css({'display':'none'})
     }
   }
 /*  Hero image hide try */
-  $(window).scroll(function() {
+/*  
+$(window).scroll(function() {
     var scroll = $(window).scrollTop();
     $("#hero-section").css({
       transform: 'translate3d(0, +'+(scroll/100)+'%, 0) scale('+(100 - scroll/50)/100+')'
     });
   });
-
+*/
 
 /* *********************************** solution cards *********************************** */
   (function($) {
@@ -95,14 +138,54 @@ $('.rounded').tilt();
 /* set to section-test scroll*/
 $('.scroll-container').scroll(function() {
   if ($('.scroll-container').scrollTop() > 100) {
-    $('.navbar-d').addClass('color-change');
+    $('.navbar').addClass('color-change');
     $('.nav-content-center').addClass('color-change');
     $('.nav-small').css({'display':'block','z-index': '2','left': '20px'})
   } else {
-    $('.navbar-d').removeClass('color-change');
+    $('.navbar').removeClass('color-change');
     $('.nav-content-center').removeClass('color-change');
     $('.nav-small').css({'display':'none'})
   }
 });
+
+// nav bar OLD
+
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarMenu = document.querySelector(".navbar ul");
+const navbarLinks = document.querySelectorAll(".navbar a");
+
+navbarToggler.addEventListener("click", navbarTogglerClick);
+
+function navbarTogglerClick() {
+  navbarToggler.classList.toggle("open-navbar-toggler");
+  navbarMenu.classList.toggle("open");
+}
+
+navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
+
+function navbarLinkClick() {
+  if(navbarMenu.classList.contains("open")) {
+    navbarToggler.click();
+  }
+}
+
+/*
+// nav bar new 
+function toggleDropdown (e) {
+  const _d = $(e.target).closest('.dropdown'),
+    _m = $('.dropdown-menu', _d);
+  setTimeout(function(){
+    const shouldOpen = e.type !== 'click' && _d.is(':hover');
+    _m.toggleClass('show', shouldOpen);
+    _d.toggleClass('show', shouldOpen);
+    $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+  }, e.type === 'mouseleave' ? 300 : 0);
+}
+
+$('body')
+  .on('mouseenter mouseleave','.dropdown',toggleDropdown)
+  .on('click', '.dropdown-menu a', toggleDropdown);
+
+*/
 
 
